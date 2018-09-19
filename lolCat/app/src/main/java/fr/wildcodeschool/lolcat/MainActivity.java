709cd471e0749ac39ef2facc2c3e9fd06e21de08
Romain Marca <rmarca@hotmail.fr>
2018-09-19 16:55:33 +0200

    Finish application

--- lolCat/app/src/main/java/fr/wildcodeschool/lolcat/MainActivity.java
+++ lolCat/app/src/main/java/fr/wildcodeschool/lolcat/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.wildcodeschool.lolcat;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final MediaPlayer meow = MediaPlayer.create(this, R.raw.meow);
+        final MediaPlayer ronron = MediaPlayer.create(this, R.raw.ronron);
+        TextView touchMe = findViewById(R.id.text_touch_me);
+        final TextView hahaha = findViewById(R.id.text_haha);
+        ImageView cat = findViewById(R.id.imgcat);
+
+
+        ronron.start();
+
+        cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ronron.stop();
+                meow.start();
+
+                hahaha.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
